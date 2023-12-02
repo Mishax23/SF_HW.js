ea11c9d96c46e1f4429adae570d4d66faebf3ae9
Mishax23 <mihakotiha@gmail.com>
2023-12-03 02:12:14 +0300

    commit

--- alert_ch1.js
+++ alert_ch1.js
@@ -1,54 +1,10 @@
-/*dollar= prompt("Внесите сумму в долорах");
+dollar= prompt("Внесите сумму в долорах");
 kurs= dollar * 91;
-alert("Перевод составил:" + kurs + "rub");*/
+alert("Перевод составил:" + kurs + "rub");
 
 
-//let nr1= 7;
-//console.log("nr1", typeof nr1);
 
-
-/*Boolean([123]);
-console.log(1 || 0);*/
-
-/*var a = 1;
-var b= 0;
-console.log(a && b);*/
-
-/*var bankBalance= 1000;
-var price= 10999;
-
-if (price <= bankBalance) {
-    console.log("Да , вы можете себе его позволить!");
-}
-else if (price < 10000) {
-    console.log("Пополните баланс");
-}
-else {
-    console.log("Нам не нужен телефон");
-
-}
-
-//console.log("Нет");*/
-
-
-const a = 1;
-const b = 2; 
-const result = a + b;
-
-/*switch (result) {
-    case 0:
-        console.log('0');
-        break;
-    case 1:
-        console.log('1');
-        break;
-    case 2:
-        console.log('2');
-        //break;
- case 3:
-        console.log('3');
-        break;
-    default:
-        console.log('1,2,3');
-}*/
+const text = "Hello";
+const result = text. split(''). reverse(). join('');
+console.log(result);
 
